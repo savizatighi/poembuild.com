3bb38b3a21a4fb1caa9ca469065002ba672a5efc
Saviz Atighi <saviz@Savizs-M1.local>
2021-05-08 16:04:54 -0500

    first changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import TextField from '@material-ui/core/TextField';
 import logo from './logo.svg';
 import './App.css';
+
+const getRhymes = ((word) => {
+  fetch(`https://api.datamuse.com/words?rel_rhy=${word}`)
+  .then(response => response.json())
+  .then(data => console.log(data));
+})
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="poembuildlogo.png"/>
+      <TextField
+          id="outlined-textarea"
+          label="Multiline Placeholder"
+          placeholder="type"
+          multiline
+          variant="outlined"
+          class="input"
+          onChange={(event) => getRhymes(event.target.value)}
+        />
       </header>
     </div>
   );
 }
 
 export default App;
+<p>hi web</p>
