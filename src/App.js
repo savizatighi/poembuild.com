404b28159a0252907dfd7da17c5abb0604924cde
Saviz Atighi <saviz@Savizs-M1.local>
2021-05-09 20:57:42 -0500

    words on web

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import TextField from '@material-ui/core/TextField';
+import React, { useState } from 'react';
+ import {TextField, List, ListItem} from '@material-ui/core';
 import './App.css';
 
-const getRhymes = ((word) => {
-  fetch(`https://api.datamuse.com/words?rel_rhy=${word}`)
-  .then(response => response.json())
-  .then(data => console.log(data));
-})
-
 function App() {
+  const [state, setState] = useState([]);
+  const getRhymes = ((word) => {
+    fetch(`https://api.datamuse.com/words?rel_rhy=${word}`)
+    .then(response => response.json())
+    .then(data => setState(data));
+  })
+  console.log(state)
   return (
     <div className="App">
       <header className="App-header">
       <TextField
           id="outlined-textarea"
-          placeholder="Write Your Poem"
+          label="Multiline Placeholder"
+          placeholder="type"
           multiline
           variant="outlined"
-          className="input"
+          class="input"
           onChange={(event) => getRhymes(event.target.value)}
         />
+        <List>
+          {state.map((rhyme) => <ListItem>{rhyme.word}</ListItem>)}
+        </List>
       </header>
     </div>
   );
